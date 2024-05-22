0a36be5ffb14698fa1c2c4bc52fe4a08902b4a92
gusyaraaa <gusyaraaa@yandex.ru>
2024-05-22 09:43:03 +0200

    deploy to mainnet

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,11 @@ import { Buffer } from 'buffer'
 window.Buffer = Buffer
 
 export const WC_PROJECT_ID = import.meta.env.VITE_WC_PROJECT_ID ?? ''
-export const DEFAULT_CHAIN = import.meta.env.DEFAULT_CHAIN ?? mainnet.id
-export const SUPPORTED_CHAINS = [mainnet.id, sepolia.id]
-export const TEST_MODE = import.meta.env.TEST_MODE ?? false
+export const DEFAULT_CHAIN = import.meta.env.VITE_DEFAULT_CHAIN ?? mainnet.id
+export const TEST_MODE = import.meta.env.VITE_TEST_MODE ?? false
 export const VITE_GET_PROOF_API_URL =
   import.meta.env.VITE_GET_PROOF_API_URL ?? ''
+
+export const SUPPORTED_CHAINS: number[] = !TEST_MODE
+  ? [mainnet.id]
+  : [sepolia.id]
--- src/modules/contracts/contracts/core.ts
+++ src/modules/contracts/contracts/core.ts
@@ -2,14 +2,19 @@ import { mainnet, sepolia } from 'wagmi/chains'
 
 import { createContractHelpers } from 'modules/contracts/utils/createContractHelpers'
 import * as generated from 'generated'
+import { SUPPORTED_CHAINS } from 'config'
 
 export const ContractRociConverter = createContractHelpers({
   name: 'RociConverter',
   factory: generated.RociConverter__factory,
   version: '1.0.0',
   address: {
-    [mainnet.id]: '',
-    [sepolia.id]: '0xbF74Cef2051B7DdbCFE4868149feBAD00f1C120D',
+    [mainnet.id]: SUPPORTED_CHAINS.includes(mainnet.id)
+      ? '0x3E5715bB5d83cf4b127CD7DE6125E86Ab3a5934C'
+      : '',
+    [sepolia.id]: SUPPORTED_CHAINS.includes(sepolia.id)
+      ? '0xbF74Cef2051B7DdbCFE4868149feBAD00f1C120D'
+      : '',
   },
   testAddress: {
     [sepolia.id]: '0xbF74Cef2051B7DdbCFE4868149feBAD00f1C120D',
